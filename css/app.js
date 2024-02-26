6e4aa90c963a81c551a1f2dc18fbf04bf389dce2
Karl <2314028@bdeb.qc.ca>
2024-02-26 17:40:29 -0500

    Created banner avec java script

--- css/app.js
+++ css/app.js
@@ -38,3 +38,57 @@ window.addEventListener('DOMContentLoaded', ()=>{
 
     })
 })
+
+/*hell*/ 
+
+const myslide = document.querySelectorAll('.myslider'),
+dots = document.querySelectorAll('.dots');
+
+let counter = 1;
+slidefun(counter);
+
+let timer = setInterval(autoslide, 8000);
+
+function autoslide(){
+    counter+=1;
+    slidefun(counter);
+}
+function plusSlides(n){
+    counter+=n;
+    slidefun(counter);
+    resetTimer();
+}
+
+function currentSlide(n){
+
+    counter = n;
+    slidefun(counter);
+    resetTimer();
+}
+
+function resetTimer(){
+    clearInterval(timer);
+    timer= setInterval(autoslide,8000);
+}
+
+function slidefun(n) {
+    let i;
+    for (i = 0; i < myslide.length; i++) {
+        myslide[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].classList.remove('active');
+    }
+    if (n > myslide.length) {
+        counter = 1;
+    } else if (n < 1) {
+        counter = myslide.length;
+    }
+    myslide[counter - 1].style.display = "block";
+    dots[counter - 1].className += " active";
+}
+
+document.querySelector('.prev').addEventListener('click', () => plusSlides(-1));
+document.querySelector('.next').addEventListener('click', () => plusSlides(1));
+
+
